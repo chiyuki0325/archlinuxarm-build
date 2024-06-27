2bb591c305f7317b95d74dac199cd24dd8976d22
Kirikaze Chiyuki <me@chyk.ink>
2024-06-27 18:10:08 +0800

    fsck gpg

--- initialize.sh
+++ initialize.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-echo "[archlinuxcn]" >> /etc/pacman.conf
-echo "Server = https://repo.archlinuxcn.org/aarch64" >> /etc/pacman.conf
-pacman-key --init
-pacman-key --populate archlinuxarm
-pacman-key --lsign-key "farseerfc@archlinux.org"
-pacman -Syu archlinux-keyring archlinuxarm-keyring archlinuxcn-keyring
-pacman -Syu --noconfirm yay
+sed -i 's/^SigLevel.*/SigLevel = Never/' /etc/pacman.conf
+sed -i 's/^LocalFileSigLevel.*/LocalFileSigLevel = Never/' /etc/pacman.conf
+cat << EOM > /etc/pacman.conf
+RemoteFileSigLevel = Never
+
+[archlinuxcn]
+Server = https://repo.archlinuxcn.org/aarch64
+SigLevel = Never
+EOM
+# pacman-key --init
+# pacman-key --populate archlinuxarm
+# pacman-key --lsign-key "farseerfc@archlinux.org"
+# pacman -Syu archlinux-keyring archlinuxarm-keyring archlinuxcn-keyring
+pacman -Syu --noconfirm base-devel git python yay
 
 useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
